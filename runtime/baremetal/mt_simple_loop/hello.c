d5043959fd6defa65aa10acef4bdbe20e0038894
chuyukang <17082643+Chuyukang@users.noreply.github.com>
2023-04-16 22:40:33 +0800

    mt_simple_loop: add multi thread simple loop test case

---
+++ runtime/baremetal/mt_simple_loop/hello.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "crt.h"
+#include "hostcalls.h"
+
+#define MSG "checkpoint test with multi-thread... \n"
+
+void sig_handler() {
+	host_save();
+}
+
+void print_a()
+{
+	for (int i = 0; i < 1500; i++)
+	{
+		host_write("a", 1);
+	}
+
+	host_save();
+
+	for (int i = 0; i < 100; i++)
+	{
+		host_write("a", 1);
+	}
+}
+
+void print_b()
+{
+	for (;;) {
+    }
+	host_exit();
+}
+
+void print_c()
+{
+	for (;;) {
+	}
+	host_exit();
+}
+
+void hello_c()
+{
+	long tid_1 = host_pthread_create(print_b, NULL);
+	long tid_2 = host_pthread_create(print_c, NULL);
+
+	print_a();
+
+	host_exit();
+}
